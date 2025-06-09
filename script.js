8bb280708dcb030964615863e8c8546efe872cde
VaibhavMP <vaibhav.panchal2k4@gmail.com>
2025-06-09 13:12:44 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function openLightbox(src) {
+  const lightbox = document.getElementById('lightbox');
+  const img = document.getElementById('lightbox-img');
+  img.src = src;
+  lightbox.style.display = 'flex';
+}
+
+function closeLightbox() {
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'none';
+}
